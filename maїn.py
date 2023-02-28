9f58594eea808d940483fd460f5b9fbde13fc41a
kuzxsophya <126552921+kuzxsophya@users.noreply.github.com>
2023-02-28 09:35:21 +0200

    Add files via upload

---
+++ maїn.py
@@ -0,0 +1,23 @@
+from tkinter import
+from tkinter import messagebox
+import pickle
+
+
+def registration()
+    Label_error = None
+
+
+frame = Frame(root, bd=10
+frame.place(relx=0.15, rely=0.2, relwidth=0.7, relheight=0.6)
+
+Label = Label(frame, text='Sing up', font='16'
+Label.place(relwidht=1, relheight=0.1)
+
+Label_login = Label(frame, text="Login:")
+# Label_logi.place(relx=0.1, rely=0.2)
+Label_login.place(rely=0.2, relwigth=0.35, relweidth=0.1)
+
+
+def Login_form():
+    print('Login_form')
+    pass
